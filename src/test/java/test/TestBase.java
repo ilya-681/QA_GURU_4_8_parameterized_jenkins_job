6359afd7723a5c97e0d6fb51e39aa8b83d90ed3c
ilya-681 <ilya-681@ya.ru>
2021-02-21 15:02:47 +0300

    Added Listener

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,12 +1,24 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub";
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.remote = System.getProperty("remote_driver");
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
     }
 }
